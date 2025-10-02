e18931c5a4077b28e1b8871d06d334e0e79671d3
Nurtay Tolymbek <nurtay.tolymbek@netcracker.com>
2025-10-02 17:26:53 +0500

    chore(tokenverifier): change err msg

--- security/tokenverifier/verifier.go
+++ security/tokenverifier/verifier.go
@@ -67,7 +67,7 @@ func newVerifier(ctx context.Context, audience string, getToken getTokenFunc) (*
 
 	rawToken, err := getToken()
 	if err != nil {
-		return nil, fmt.Errorf("failed to get k8s projected volume token: %w", err)
+		return nil, fmt.Errorf("failed to get k8s projected volume token (possibly projected volume token misconfigured in k8s deployment): %w", err)
 	}
 	issuer, err := getIssuer(rawToken)
 	if err != nil {
