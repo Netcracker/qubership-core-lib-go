9338e9380b013588d7dd8c639b4afb501082e7f7
nurtai325 <nurtay.tolymbek@netcracker.com>
2026-07-02 09:52:27 +0500

    chore: refactoring

---
+++ security/security.go
@@ -0,0 +1,24 @@
+package security
+
+import (
+	"context"
+	"os"
+	"strconv"
+
+	"github.com/netcracker/qubership-core-lib-go/v3/security/tokensource"
+	"github.com/netcracker/qubership-core-lib-go/v3/serviceloader"
+)
+
+func GetTokenFunc() func(ctx context.Context) (string, error) {
+	rawK8sM2mEnabled := os.Getenv("KUBERNETES_M2M_ENABLED")
+	k8sM2mEnabled, err := strconv.ParseBool(rawK8sM2mEnabled)
+	if err != nil && rawK8sM2mEnabled != "" {
+		logger.Warn("failed to parse KUBERNETES_M2M_ENABLED env var: %s", rawK8sM2mEnabled)
+	}
+	if k8sM2mEnabled {
+		return func(ctx context.Context) (string, error) {
+			return tokensource.GetAudienceToken(ctx, tokensource.AudienceNetcracker)
+		}
+	}
+	return serviceloader.MustLoad[TokenProvider]().GetToken
+}
