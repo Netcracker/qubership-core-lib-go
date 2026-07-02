1923e0889139b8f0e88dfc4f9398f80cee9b79ce
nurtai325 <nurtay.tolymbek@netcracker.com>
2026-07-02 09:54:50 +0500

    chore: refactoring

--- security/security.go
+++ security/security.go
@@ -9,6 +9,7 @@ import (
 	"github.com/netcracker/qubership-core-lib-go/v3/serviceloader"
 )
 
+// GetTokenFunc returns a function to get an m2m token. If KUBERNETES_M2M_ENABLED is enabled it returns a k8s m2m token. Otherwise a keycloak token
 func GetTokenFunc() func(ctx context.Context) (string, error) {
 	rawK8sM2mEnabled := os.Getenv("KUBERNETES_M2M_ENABLED")
 	k8sM2mEnabled, err := strconv.ParseBool(rawK8sM2mEnabled)
